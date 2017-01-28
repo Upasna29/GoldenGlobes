a014cc7a4da406813fca84d74ee1721d7a81b3d8
Collin Pham <collinpham2018@u.northwestern.edu>
2017-01-27 18:15:12 -0600

    Determine host based off of twitter handle

--- host_name.py
+++ host_name.py
@@ -1,22 +1,48 @@
-f = open('globestweets.txt','r')
-f = list(f)
-usernames = list()
+import numpy
+tweets = numpy.load("tweetsarray.npy")
 
 ## filter_tweets
 # Array of Str, Array of Dicts -> Array of Dicts
 #
 # Returns an array of tweet objects (dicts) whose text component contains at least one of the key words.
 def filter_tweets(key_words, tweets):
-  relevant_tweets = {}
+  relevant_tweets = []
 
   for tweet in tweets:
     for key_word in key_words:
       # If the key word is in the text of the tweet, add it to the dict and break
       if ((key_word in tweet['tweet_text'])):
-        relevant_tweets[tweet['tweet_ID']] = tweet
+        relevant_tweets.append(tweet)
         break
   return relevant_tweets
 
-def host_name()
+def determine_host(key_words, tweets):
+  relevant_tweets = filter_tweets(key_words, tweets)
+  twitter_handles = {}
 
-host_name(['cold', 'open', 'host'], 'Hi MY name is')
+  for tweet in relevant_tweets:
+    for word in tweet['tweet_text'].split():
+      if word.startswith('@'):
+        if twitter_handles.has_key(word):
+          twitter_handles[word] += 1
+        else:
+          twitter_handles[word] = 0
+  return twitter_handles
+
+
+def max_count(twitter_handles):
+  max_count = 0
+  host_handle = ''
+
+  for twitter_handle in twitter_handles.keys():
+    if twitter_handles[twitter_handle] > max_count:
+      max_count = twitter_handles[twitter_handle]
+      host_handle = twitter_handle
+
+  return host_handle
+    
+
+possible_hosts = determine_host(['cold', 'open', 'host'], tweets)
+host = max_count(possible_hosts)
+
+print(host)
